f47540019f829a834b052e810daadde33a644fc9
Łukasz Tucholski <luk.tucholski@gmail.com>
2019-02-21 17:03:48 +0100

    history nav component

---
+++ src/components/HistoryNavigation.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="history-nav">
+    <ul>
+      <router-link
+        to=""
+        tag="li"
+        @click.native="goBack"
+      >
+        << Back
+      </router-link>
+      <router-link
+        to=""
+        tag="li"
+        @click.native="goForward"
+      >
+        Forward >>
+      </router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HistoryNavigation',
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
+    goForward() {
+      this.$router.go(1);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
