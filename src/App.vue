b0dfc4bbae1afb34d8d4a5c3332abd5a62586452
Łukasz Tucholski <luk.tucholski@gmail.com>
2019-02-21 16:04:28 +0100

    menu component and first styles

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="library-system">
     <h1 class="library-system__title">VUE APP</h1>
-    <router-view />
+    <Menu />
+    <div class="content">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
+import Menu from './components/MenuList.vue';
+
 export default {
   name: 'App',
+  components: { Menu },
 };
 </script>
 
---
+++ src/components/MenuList.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav class="menu">
+    <ul>
+      <router-link
+        to="/"
+        tag="li"
+      >
+        Home
+      </router-link>
+      <router-link
+        to="/search"
+        tag="li"
+      >
+        Search
+      </router-link>
+      <router-link
+        to="/catalog"
+        tag="li"
+      >
+        Browse Catalog
+      </router-link>
+      <router-link
+        to="/settings"
+        tag="li"
+      >
+        Display Settings
+      </router-link>
+      <router-link
+        to="/contact"
+        tag="li"
+        class="contact__link"
+      >
+        Contact
+      </router-link>
+      <router-link
+        to="/admin"
+        tag="li"
+      >
+        Admin Panel
+      </router-link>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'MenuList',
+};
+</script>
+
+<style>
+</style>
