b5e752da2fe7391a9a7ed1264f796a4080cad8f3
Łukasz Tucholski <luk.tucholski@gmail.com>
2019-02-21 17:04:20 +0100

    user menu component

---
+++ src/components/UserMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="user-menu">
+    <ul>
+      <li>
+        <a href="#/">
+          Sign In
+        </a>
+      </li>
+      <li>or</li>
+      <li>
+        <a href="#/">
+          Sign Up
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserMenu',
+};
+</script>
+
+<style>
+</style>
