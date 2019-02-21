ff2f77d01b9e0d772d935f38b7ccb1fcc1928c23
Łukasz Tucholski <luk.tucholski@gmail.com>
2019-02-21 16:43:11 +0100

    added missing views component

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Home from '../views/Home.vue';
+import Search from '../views/Search.vue';
+import Catalog from '../views/Catalog.vue';
+import Settings from '../views/DisplaySettings.vue';
+import Contact from '../views/Contact.vue';
+import AdminPanel from '../views/AdminPanel.vue';
 import Error404 from '../views/errors/404.vue';
 
 Vue.use(Router);
@@ -16,6 +21,46 @@ const router = new Router({
       component: Home,
       meta: {
         title: 'Library System by ŁT',
+      },
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: Search,
+      meta: {
+        title: 'Search in the catalog',
+      },
+    },
+    {
+      path: '/catalog',
+      name: 'catalog',
+      component: Catalog,
+      meta: {
+        title: 'Browse Catalog',
+      },
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: Settings,
+      meta: {
+        title: 'Settings',
+      },
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact,
+      meta: {
+        title: 'Contact',
+      },
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: AdminPanel,
+      meta: {
+        title: 'Admin Panel',
       },
     },
     {
---
+++ src/views/DisplaySettings.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="settings">
+    <h1>Display Settings</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non nulla nec ex ullamcorper consectetur. Nunc pellentesque dolor ac pellentesque efficitur. Vivamus et fringilla urna, sed viverra urna. Maecenas a tellus quis lorem semper tincidunt at bibendum velit. Proin sit amet lorem ex. Morbi id tortor dui. Donec nec interdum sem. Sed gravida leo justo, quis gravida nulla fringilla nec. Nunc leo velit, cursus in mollis vitae, imperdiet ac purus. Cras at metus tortor. Proin eu nibh nec lectus mattis facilisis at in ante. Etiam sit amet ultricies ex. Nullam finibus, metus sed gravida placerat, libero neque eleifend turpis, a gravida lacus elit quis est. Duis maximus iaculis est, nec convallis elit mollis vitae. In hac habitasse platea dictumst. Nulla tincidunt non nibh accumsan mattis. Aenean efficitur eleifend orci. Pellentesque fermentum sed quam a molestie. Fusce at volutpat dolor. Suspendisse id nulla vel lectus semper sodales ornare eu libero. Donec massa diam, porta eget felis nec, tristique commodo sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi at ullamcorper mi. Vestibulum urna arcu, luctus nec viverra non, ullamcorper feugiat nibh. Nam commodo cursus mauris sed euismod. Curabitur blandit volutpat eros vel bibendum. Nunc non feugiat mauris. Quisque euismod ac turpis vitae ultricies. Maecenas volutpat a leo id pretium. Proin ut odio vestibulum, feugiat odio et, scelerisque elit. Duis sollicitudin maximus lectus, ut hendrerit sapien venenatis vel. Donec bibendum lacus urna, sit amet mollis felis bibendum nec. Integer at sapien arcu.</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Settings',
+};
+</script>
+
+<style>
+</style>
