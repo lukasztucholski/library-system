fc145af09ddffbee21a3dfd2d258d9eadd166e72
Łukasz Tucholski <luk.tucholski@gmail.com>
2019-03-21 23:18:27 +0100

    sign in/up button addnotation in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,7 @@
   <footer class="footer">
     <p>Made by <a href="https://github.com/lukasztucholski">Łukasz Tucholski</a>. License: <a href="https://en.wikipedia.org/wiki/MIT_License">MIT</a></p>
     <div>
+      <p>//temp area</p>
       <button @click="wyloguj()">
         logout
       </button>
